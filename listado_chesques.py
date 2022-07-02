b36e1bc4cb12677b79d49f3617d26aed74fd4a62
thiagozur <105321698+thiagozur@users.noreply.github.com>
2022-07-02 18:09:36 -0300

    final

--- listado_chesques.py
+++ listado_chesques.py
@@ -19,6 +19,18 @@ def rangobien(r):
         else:
             pass
 
+def bisiesto(a):
+    if a % 4 == 0:
+        if a % 100 == 0:
+            if a % 400 == 0:
+                return True
+            else:
+                return False    
+        else:
+            return True
+    else:
+        return False
+
 def isrango(a):
     #revisa que no haya mas de los puntos de division de la string del rango permitidos y que no haya caracteres no permitidos
     if (a.count(":") == 1) and (a.count("-") == 4) and (rangobien(a) != False):
@@ -45,12 +57,19 @@ def isrango(a):
                         else:
                             return False
                     #comprueba si el mes de la segunda fecha tiene 28 dias        
-                    elif rangevals[1][1] == "28":
-                        #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
-                        if (not int(rangevals[1][0]) > 28) and (int(rangevals[1][0]) != 0):
-                            return True
+                    elif rangevals[1][1] == "02":
+                        if bisiesto(int(rangevals[1][2])):
+                                #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
+                                if (not int(rangevals[1][0]) > 29) and (int(rangevals[1][0]) != 0):
+                                    return True
+                                else:
+                                    return False
                         else:
-                            return False
+                            #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
+                            if (not int(rangevals[1][0]) > 28) and (int(rangevals[1][0]) != 0):
+                                return True
+                            else:
+                                return False
                     else:
                         return False
                 else:
@@ -74,45 +93,95 @@ def isrango(a):
                         else:
                             return False
                     #comprueba si el mes de la segunda fecha tiene 28 dias
-                    elif rangevals[1][1] == "28":
-                        #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
-                        if (not int(rangevals[1][0]) > 28) and (int(rangevals[1][0]) != 0):
-                            return True
+                    elif rangevals[1][1] == "02":
+                        if bisiesto(int(rangevals[1][2])):
+                                #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
+                                if (not int(rangevals[1][0]) > 29) and (int(rangevals[1][0]) != 0):
+                                    return True
+                                else:
+                                    return False
                         else:
-                            return False
+                            #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
+                            if (not int(rangevals[1][0]) > 28) and (int(rangevals[1][0]) != 0):
+                                return True
+                            else:
+                                return False
                     else:
                         return False
                 else:
                     return False
             #comprueba si el mes de la primera fecha tiene 28 dias
-            elif rangevals[0][1] == "28":
-                #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
-                if (not int(rangevals[0][0]) > 28) and (int(rangevals[0][0]) != 0):
-                    #comprueba si el mes de la segunda fecha tiene 31 dias
-                    if rangevals[1][1] in month31:
-                        #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 31 dias
-                        if (not int(rangevals[1][0]) > 31) and (int(rangevals[1][0]) != 0):
-                            return True
-                        else:
-                            return False
-                    #comprueba si el mes de la segunda fecha tiene 30 dias
-                    elif rangevals[1][1] in month30:
-                        #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 30 dias
-                        if (not int(rangevals[1][0]) > 30) and (int(rangevals[1][0]) != 0):
-                            return True
-                        else:
-                            return False
-                    #comprueba si el mes de la segunda fecha tiene 28 dias
-                    elif rangevals[1][1] == "28":
-                        #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
-                        if (not int(rangevals[1][0]) > 28) and (int(rangevals[1][0]) != 0):
-                            return True
+            elif rangevals[0][1] == "02":
+                if bisiesto(int(rangevals[0][2])):
+                    #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
+                    if (not int(rangevals[0][0]) > 29) and (int(rangevals[0][0]) != 0):
+                        #comprueba si el mes de la segunda fecha tiene 31 dias
+                        if rangevals[1][1] in month31:
+                            #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 31 dias
+                            if (not int(rangevals[1][0]) > 31) and (int(rangevals[1][0]) != 0):
+                                return True
+                            else:
+                                return False
+                        #comprueba si el mes de la segunda fecha tiene 30 dias
+                        elif rangevals[1][1] in month30:
+                            #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 30 dias
+                            if (not int(rangevals[1][0]) > 30) and (int(rangevals[1][0]) != 0):
+                                return True
+                            else:
+                                return False
+                        #comprueba si el mes de la segunda fecha tiene 28 dias
+                        elif rangevals[1][1] == "02":
+                            if bisiesto(int(rangevals[1][2])):
+                                #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
+                                if (not int(rangevals[1][0]) > 29) and (int(rangevals[1][0]) != 0):
+                                    return True
+                                else:
+                                    return False
+                            else:
+                                #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
+                                if (not int(rangevals[1][0]) > 28) and (int(rangevals[1][0]) != 0):
+                                    return True
+                                else:
+                                    return False
                         else:
                             return False
                     else:
                         return False
                 else:
-                    return False
+                    #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
+                    if (not int(rangevals[0][0]) > 28) and (int(rangevals[0][0]) != 0):
+                        #comprueba si el mes de la segunda fecha tiene 31 dias
+                        if rangevals[1][1] in month31:
+                            #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 31 dias
+                            if (not int(rangevals[1][0]) > 31) and (int(rangevals[1][0]) != 0):
+                                return True
+                            else:
+                                return False
+                        #comprueba si el mes de la segunda fecha tiene 30 dias
+                        elif rangevals[1][1] in month30:
+                            #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 30 dias
+                            if (not int(rangevals[1][0]) > 30) and (int(rangevals[1][0]) != 0):
+                                return True
+                            else:
+                                return False
+                        #comprueba si el mes de la segunda fecha tiene 28 dias
+                        elif rangevals[1][1] == "02":
+                            if bisiesto(int(rangevals[1][2])):
+                                #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
+                                if (not int(rangevals[1][0]) > 29) and (int(rangevals[1][0]) != 0):
+                                    return True
+                                else:
+                                    return False
+                            else:
+                                #comprueba que la cantidad de dias proporcionada sea adecuada para un mes de 28 dias
+                                if (not int(rangevals[1][0]) > 28) and (int(rangevals[1][0]) != 0):
+                                    return True
+                                else:
+                                    return False
+                        else:
+                            return False
+                    else:
+                        return False
             else:
                 return False
         else: return False
@@ -211,6 +280,28 @@ if rango:
     datefrom = datetime.datetime.strptime(rangedates[0], "%d-%m-%Y")#.strftime('%d-%m-%Y')
     dateto = datetime.datetime.strptime(rangedates[1], "%d-%m-%Y")#.strftime('%d-%m-%Y')
 
+def rep(index):
+    cheqrevisados = [] #lista a la que se le agregan los numeros de cheques no repetidos
+    cta = origenes[index[0]] #lista a la que se le agregan los numeros de cuentas repetidas
+    cheqrep = False
+    ctadist = False
+    for i in index:
+        if cheques[i] not in cheqrevisados:
+            cheqrevisados.append(cheques[i])
+        else:
+            cheqrep = True
+    for i in index:
+        if origenes[i] != cta:
+            ctadist = True   
+    if cheqrep and ctadist:
+        salir("Error del sistema: este DNI tiene más de un número de cuenta asociado y sus números de cheque se repiten al menos una vez")
+    elif cheqrep and not ctadist:
+        salir("Error del sistema: para este DNI, los números de cheque se repiten al menos una vez")
+    elif ctadist and not cheqrep:
+        salir("Error del sistema: este DNI tiene más de un número de cuenta asociado")
+        
+    
+
 def makeindex(state=None, rang=None):
     indicesdni = [i for i, x in enumerate(dnis) if x == dni]
     typeindex = [i for i, x in enumerate(tipos) if x == tipo]
@@ -242,6 +333,8 @@ def makeindex(state=None, rang=None):
 def dispscreen(state=None, rang=None):
     checkn = 0
     resultindex = makeindex(state, rang)
+    dnindex = [i for i, x in enumerate(dnis) if x == dni]
+    rep(dnindex)
     for i in resultindex:
         checkn = checkn + 1
         print(f'Cheque {checkn}:\nFecha de emisión: {datetime.datetime.fromtimestamp(int(fechaso[i-1])).strftime("%d-%m-%Y")}\nFecha de pago/cobro: {datetime.datetime.fromtimestamp(int(fechasp[i-1])).strftime("%d-%m-%Y")}\nValor del cheque: {valores[i]}\nNúmero de cuenta: {origenes[i]}')
@@ -250,7 +343,8 @@ def dispscreen(state=None, rang=None):
 def expcsv(state=None, rang=None):  
     checkn = 0
     resultindex = makeindex(state, rang)
-    fechahora = str(datetime.datetime.now()).split('.')[0].replace(":", ",")
+    dnindex = [i for i, x in enumerate(dnis) if x == dni]
+    rep(dnindex)
     arc = open(f".\\csv_exportados\\DNI_{dnis[resultindex[0]]} TIMESTAMP_{int(datetime.datetime.now().timestamp())}.csv", 'w')
     arc.writelines(f"Cheque,Fecha de emision,Fecha de pago/cobro,Valor del cheque,Numero de cuenta\n")
     for i in resultindex:
